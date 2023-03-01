02da0e071317b2515fa0f850a304727a887b6c65
ramya862 <103271729+ramya862@users.noreply.github.com>
2023-03-01 17:28:55 +0530

    Create Evenorodd.cs

---
+++ Evenorodd.cs
@@ -0,0 +1,19 @@
+using System;
+namespace myapp
+{
+class Main1
+{
+static void Main(string[] args)
+{
+int x=10;
+if(x%2==0)
+{
+Console.WriteLine("The number x is even");
+}
+else
+{
+Console.WriteLine("The number x is odd");
+}
+}
+}
+}
