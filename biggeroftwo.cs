653fc8b843a5a1c819ce8dfb2dfbfa54c8e93856
ramya862 <103271729+ramya862@users.noreply.github.com>
2023-03-01 16:55:44 +0530

    Create biggeroftwo.cs

---
+++ biggeroftwo.cs
@@ -0,0 +1,20 @@
+using System;
+namespace myapp
+{
+class hey
+{
+static void Main(string[] args)
+{
+int x=10;
+int y=5;
+if(x>y)
+{
+Console.WriteLine("X is greater than y");
+}
+else
+{
+Console.WriteLine("X is less than y");
+}
+}
+}
+}
